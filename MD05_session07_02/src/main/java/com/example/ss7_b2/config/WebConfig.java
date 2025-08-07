710683c00b72fdcb6e73aa9d12bcfb8b14aeda93
anhduong1608 <anhduong160821@gmail.com>
2025-08-07 23:53:05 +0700

    Add files via upload

---
+++ MD05_session07_02/src/main/java/com/example/ss7_b2/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.ss7_b2.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.ss7_b2")
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/views/", ".jsp");
+    }
+}
